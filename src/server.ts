44703abcfb5b9c82995983390d4d93b2f180b810
vitorbrambilla <vitorbrambilla10@gmail.com>
2022-05-06 10:45:14 -0300

    Add port and variable

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import { routes } from './routes';
 
-const host = 'http://localhost';
-const port = 3333;
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(port, () => {
-  console.log(`Server running at ${host}:${port}`)
+app.listen(process.env.PORT || 3333, () => {
+  console.log(`Server running`)
 });
